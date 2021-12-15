2481406017a9c957d485c1aaa5c77f545eb2ea8c
Marina Lyaleko <M_hodanenok@mail.ru>
2021-12-15 08:17:22 +0300

    refactored ColorTest

--- src/test/java/tests/ColorTest.java
+++ src/test/java/tests/ColorTest.java
@@ -2,29 +2,19 @@ package tests;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
-import steps.BasicBookingSteps;
+import steps.BookingBeforeAfter;
 
-public class ColorTest extends BasicBookingSteps {
+public class ColorTest extends BookingBeforeAfter {
 
     @Test
     public void titleColorTest(){
 
-        driver.findElement(By.xpath(page.searchPlaceInput)).sendKeys("Москва");
-        driver.findElement(By.xpath(page.checkPricesButton)).click();
+        homePage.goToBookingMain();
+        homePage.searchCity("Москва");
+        resultPage.jsScrollToElement(resultPage.getDefiniteCard(10));
+        resultPage.jsColorCard("green", resultPage.getDefiniteCard(10));
+        resultPage.jsColorText("red", resultPage.getDefiniteCard(10));
 
-        WebElement tenthElement = driver.findElement(By.xpath(page.boxTenthsResult));
-        WebElement elementTitle = driver.findElement(By.xpath(page.titleTenthsResult));
-
-        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true)", tenthElement);
-        ((JavascriptExecutor)driver).executeScript("arguments[0].style.backgroundColor = 'green'", tenthElement);
-        ((JavascriptExecutor)driver).executeScript("arguments[0].style.color = 'red'", elementTitle);
-
-        WebElement actualRedElement = driver.findElement(By.xpath(page.titleTenthsResult));
-        String redActualRedElement = actualRedElement.getCssValue("color");
-
-        Assert.assertEquals("Result isn't correct", "rgba(255, 0, 0, 1)", redActualRedElement);
+        Assert.assertEquals("Result isn't correct", "rgba(255, 0, 0, 1)", resultPage.getDefiniteCard(10).getCssValue("color"));
     }
 }
