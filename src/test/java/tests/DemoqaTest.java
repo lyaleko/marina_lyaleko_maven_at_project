5a6f6faf5e766fe4f18b58a9618472b66854c4b6
Marina Lyaleko <M_hodanenok@mail.ru>
2021-12-15 08:19:01 +0300

    refactored DemoqaTest, TODO continue refactor

--- src/test/java/tests/DemoqaTest.java
+++ src/test/java/tests/DemoqaTest.java
@@ -8,42 +8,42 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
-import pages.DemoqaPage;
+import pages.demoqa.DemoqaPage;
 
 import java.util.concurrent.TimeUnit;
 
 public class DemoqaTest {
 
     WebDriver driver = new ChromeDriver();
-    DemoqaPage page = new DemoqaPage();
+    DemoqaPage demoPage = new DemoqaPage();
 
     @Before
     public void before(){
         setTimeouts(30);
         driver.manage().window().maximize();
-        driver.navigate().to("https://demoqa.com/select-menu");
+        driver.navigate().to(demoPage.DEMOQA_SITE);
     }
 
     @Test
     public void selectValueTest(){
 
-        driver.findElement(By.xpath(page.inputSelectValue)).click();
+        driver.findElement(By.xpath(demoPage.SELECT_VALUE_INPUT)).click();
         driver.findElement(By.xpath("//div[contains(text(),'Group 1, option 2')]")).click();
-        driver.findElement(By.xpath(page.inputSelectOne)).click();
+        driver.findElement(By.xpath(demoPage.SELECT_ONE_INPUT)).click();
         driver.findElement(By.xpath("//div[contains(text(),'Dr.')]")).click();
 
-        WebElement element = driver.findElement(By.xpath(page.selectOldStyle));
+        WebElement element = driver.findElement(By.xpath(demoPage.OLD_STYLE_SELECT));
 
         Select select = new Select(element);
         select.selectByVisibleText("Aqua");
 
         ((JavascriptExecutor)driver).executeScript(("window.scrollBy(0,1000)"));
 
-        driver.findElement(By.xpath(page.inputMultiselect)).click();
+        driver.findElement(By.xpath(demoPage.MULTISELECT_INPUT)).click();
 
         driver.findElement(By.xpath("//div[contains(text(),'Green')]")).click();
 
-        element = driver.findElement(By.xpath(page.selectCars));
+        element = driver.findElement(By.xpath(demoPage.CARS_SELECT));
         select = new Select(element);
         select.selectByValue("saab");
     }
