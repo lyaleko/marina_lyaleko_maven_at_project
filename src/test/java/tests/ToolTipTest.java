787d1025253f634d4db5d5815b15a2f92d494880
Marina Lyaleko <M_hodanenok@mail.ru>
2021-12-15 08:24:19 +0300

    refactored ToolTipTest

--- src/test/java/tests/ToolTipTest.java
+++ src/test/java/tests/ToolTipTest.java
@@ -5,26 +5,29 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import steps.BasicBookingSteps;
+import pages.booking.BookingHomePage;
+import pages.booking.BookingResultPage;
+import steps.BookingBeforeAfter;
 
-public class ToolTipTest extends BasicBookingSteps {
+public class ToolTipTest extends BookingBeforeAfter {
+
 
     @Test
     public void toolTipTest() {
 
-        WebElement element = driver.findElement(By.xpath(page.selectCurrency));
+        WebElement element = driver.findElement(By.xpath(homePage.CURRENCY_BUTTON));
 
         Actions actions = new Actions(driver);
         actions.moveToElement(element);
         actions.perform();
 
-        Assert.assertEquals("Incorrect text Tooltip Currency ", "Выберите валюту", driver.findElement(By.xpath(page.textTooltip)).getText());
+        Assert.assertEquals("Incorrect text Tooltip Currency ", "Выберите валюту", driver.findElement(By.xpath(homePage.TOOLTIP_TEXT)).getText());
 
-        element = driver.findElement(By.xpath(page.selectLanguage));
+        element = driver.findElement(By.xpath(homePage.LANGUAGE_BUTTON));
 
         actions.moveToElement(element);
         actions.perform();
 
-        Assert.assertEquals("Incorrect text Tooltip Language ", "Выберите язык", driver.findElement(By.xpath(page.textTooltip)).getText());
+        Assert.assertEquals("Incorrect text Tooltip Language ", "Выберите язык", driver.findElement(By.xpath(homePage.TOOLTIP_TEXT)).getText());
     }
 }
