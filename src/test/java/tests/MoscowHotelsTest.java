76d2df346ff37e3f72933b60daa654df77c3f70a
Marina Lyaleko <M_hodanenok@mail.ru>
2021-12-15 08:23:31 +0300

    refactored MoscowHotelsTest

--- src/test/java/tests/MoscowHotelsTest.java
+++ src/test/java/tests/MoscowHotelsTest.java
@@ -2,29 +2,18 @@ package tests;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import steps.BasicBookingSteps;
+import steps.BookingBeforeAfter;
 
-public class MoscowHotelsTest extends BasicBookingSteps {
+public class MoscowHotelsTest extends BookingBeforeAfter {
 
     @Test
-    public void isHotelsTest() {
+    public void isHotelExistTest() {
 
-        driver.findElement(By.xpath(page.searchPlaceInput)).sendKeys("Москва");
-        driver.findElement(By.xpath(page.calendarRange)).click();
-        driver.findElement(By.xpath(String.format("//td[@data-date='%s']", this.getDateString(0)) )).click();
-        driver.findElement(By.xpath(String.format("//td[@data-date='%s']", this.getDateString(7)) )).click();
-        driver.findElement(By.xpath(page.checkPricesButton)).click();
+        homePage.goToBookingMain();
+        homePage.inputCity("Москва");
+        homePage.setDatesCalendar(0,7);
+        homePage.submitSearch();
 
-        WebElement webElement = new WebDriverWait(driver, 1000).until(
-                ExpectedConditions.elementToBeClickable (By.xpath("//a[contains(text(), 'сначала самая низкая')]")));
-                webElement.click();
-
-        WebElement firstElement = driver.findElement(By.xpath("//div[@data-testid='property-card'][1]"));
-
-        Assert.assertTrue(firstElement.isDisplayed());
+        Assert.assertTrue(resultPage.getDefiniteCard(1).isDisplayed());
     }
 }
