4e0bd42a434057a75942d8777c30dda842392b7d
Marina Lyaleko <M_hodanenok@mail.ru>
2021-12-15 08:16:17 +0300

    changed type of variables in GooglePage

---
+++ src/test/java/pages/google/GooglePage.java
@@ -0,0 +1,7 @@
+package pages.google;
+
+public class GooglePage {
+
+    public static final String GOOGLE_SEARCH = "//input[@name = 'q']";
+    public static final String RESULT_BOX = "//div[@class = 'g']";
+}
