c2a6aa8ebc747f7c3f0505d3461b2a2db40e4c07
Marina Lyaleko <M_hodanenok@mail.ru>
2021-12-15 08:22:02 +0300

    refactored MaxHotelRaitingTest

--- src/test/java/tests/MaxHotelRatingTest.java
+++ src/test/java/tests/MaxHotelRatingTest.java
@@ -2,31 +2,21 @@ package tests;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import steps.BasicBookingSteps;
+import steps.BookingBeforeAfter;
 
-public class MaxHotelRatingTest extends BasicBookingSteps {
+public class MaxHotelRatingTest extends BookingBeforeAfter {
 
     @Test
-    public void findMaxRaiting(){
+    public void findMaxRating(){
 
-        driver.findElement(By.xpath(page.searchPlaceInput)).sendKeys("Москва");
-        driver.findElement(By.xpath(page.calendarRange)).click();
-        driver.findElement(By.xpath(String.format("//td[@data-date='%s']", this.getDateString(0)) )).click();
-        driver.findElement(By.xpath(String.format("//td[@data-date='%s']", this.getDateString(7)) )).click();
-        driver.findElement(By.xpath(page.checkPricesButton)).click();
-        driver.findElement(By.xpath(page.menuExtraSorting)).click();
-        driver.findElement(By.xpath(page.sortingByScorePrice)).click();
+        homePage.goToBookingMain();
+        homePage.inputCity("Москва");
+        homePage.setDatesCalendar(0,7);
+        homePage.submitSearch();
+        resultPage.openExtraMenu();
+        resultPage.sortByScorePrice();
+        wait.waitForElement(resultPage.SPINNER);
 
-        WebElement webElement = new WebDriverWait(driver, 2000).until(
-                ExpectedConditions.elementToBeClickable (By.xpath("//div[@data-testid='property-card'][1]//div[@aria-label='Оценка 10']")));
-
-        String maxPriceTextRating = webElement.getText();
-        int maxPriceNumberRating = Integer.parseInt(maxPriceTextRating.replaceAll("\\D+", ""));
-
-        Assert.assertTrue("The highest hotel rating is incorrect ", maxPriceNumberRating >= 9);
-    }
+       Assert.assertTrue("The highest hotel rating is incorrect ", resultPage.getMaxPriceRating() >= 9);
+   }
 }
