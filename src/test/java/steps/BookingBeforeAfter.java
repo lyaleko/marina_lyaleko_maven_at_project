2d87da0ab0d3e93fb38aa1c4c5a8b098b55ad242
Marina Lyaleko <M_hodanenok@mail.ru>
2021-12-18 00:39:06 +0300

    changed var names in BookingBeforeAfter

--- src/test/java/steps/BookingBeforeAfter.java
+++ src/test/java/steps/BookingBeforeAfter.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebDriver;
 import pages.booking.BookingHomePage;
 import pages.booking.BookingResultPage;
 import pages.booking.BookingSignInPage;
+import pages.trashmail.TrashMailGridPage;
+import pages.trashmail.TrashMailHomePage;
 import utils.Actions;
 import utils.ExplicitWait;
 import java.util.concurrent.TimeUnit;
@@ -13,11 +15,13 @@ import java.util.concurrent.TimeUnit;
 public abstract class BookingBeforeAfter {
 
     protected WebDriver driver = BasicDriver.getWebDriver();
-    protected BookingHomePage homePage = new BookingHomePage(BasicDriver.getWebDriver());
-    protected BookingResultPage resultPage = new BookingResultPage(BasicDriver.getWebDriver());
+    protected BookingHomePage bookingHomePage = new BookingHomePage(BasicDriver.getWebDriver());
+    protected BookingResultPage bookingResultPage = new BookingResultPage(BasicDriver.getWebDriver());
     protected ExplicitWait wait = new ExplicitWait();
-    protected BookingSignInPage signInPage = new BookingSignInPage(BasicDriver.getWebDriver());
+    protected BookingSignInPage bookingSignInPage = new BookingSignInPage(BasicDriver.getWebDriver());
     protected Actions actions = new Actions(BasicDriver.getWebDriver());
+    protected TrashMailHomePage trashMailHomePage = new TrashMailHomePage(BasicDriver.getWebDriver());
+    protected TrashMailGridPage trashMailGridPage = new TrashMailGridPage(BasicDriver.getWebDriver());
 
     @Before
     public void before(){
